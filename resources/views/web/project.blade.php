6f39c5a91c48379a39b9d5dab7f5b492fefedb4d
jpdrsanchez <joaopedrosanchez302@gmail.com>
2022-09-06 18:13:08 -0300

    fix gallery var

--- resources/views/web/project.blade.php
+++ resources/views/web/project.blade.php
@@ -1,3 +1,3 @@
 <x-web.templates.base title="{{ $project->title }}">
-  <x-web.templates.single-schedule previous-title="Evento" :previous-route="route('web.projects')" :page-title="$project->title" :page-content="$project->description" :page-photos="$project->media" />
+  <x-web.templates.single-schedule previous-title="Evento" :previous-route="route('web.projects')" :page-title="$project->title" :page-content="$project->description" :page-photos="$project->gallery->media" />
 </x-web.templates.base>
