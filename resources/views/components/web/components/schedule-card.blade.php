5f2841e3e9aab3b49fecfe938d850e34ae0a6eaa
jpdrsanchez <joaopedrosanchez302@gmail.com>
2022-09-06 17:19:07 -0300

    unescape html

--- resources/views/components/web/components/schedule-card.blade.php
+++ resources/views/components/web/components/schedule-card.blade.php
@@ -4,7 +4,7 @@
   <img src="{{ $background }}" alt="" aria-hidden="true" class="schedule-card__image">
   <div class="schedule-card__inner">
     <h3 class="schedule-card__title">{{ $title }}</h3>
-    <p class="schedule-card__text">{{ $content }}</p>
+    <div class="schedule-card__text">{!! $content !!}</div>
     <a href="{{ $link }}" class="schedule-card__button">
       Saiba mais
     </a>
