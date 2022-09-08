bc043cd72cc3c3a14ce6f6bc5d24aab76fb7d8a3
jpdrsanchez <joaopedrosanchez302@gmail.com>
2022-09-08 00:17:49 -0300

    fix error

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1,4 +1,4 @@
-<x-web.templates.base :title="$homepage->title_seo" :description="$homepage->description_seo">
+<x-web.templates.base :description="$homepage->description_seo" :title="$homepage->title_seo">
   @unless ($banners->isEmpty())
   <x-web.components.home-carousel>
     @foreach ($banners as $banner)
